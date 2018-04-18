7ab971fae00cbdf68fdd64e423409615abed6f5f
fractalbach <4bach@protonmail.com>
2018-04-18 03:49:27 -0700

    added more features to GoGetGo
    - changes directories to user.
    - removes downloaded archive clutter.
    - adds user's go bin to path.

--- scripts/GoGetGo.sh
+++ scripts/GoGetGo.sh
@@ -5,13 +5,19 @@ VERSION=1.10.1
 OS=linux
 ARCH=amd64
 
+# Change directory to user's home.
+cd ~
+
 # Download the archived Go.
 sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz -o go$VERSION.$OS-$ARCH.tar.gz
 
 # Unpack the Archive, placing it in /usr/local.
 sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
 
+# Remove the archive that was previously downloaded.
+sudo rm go$VERSION.$OS-$ARCH.tar.gz
+
 # Add go to the path.
 cat <<EOT >> ~/.profile
-export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/usr/local/go/bin:~/go/bin
 EOT
