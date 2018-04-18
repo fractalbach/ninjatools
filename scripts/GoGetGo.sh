e19d13f4c0d1aae659514d748739fa69b4083650
fractalbach <4bach@protonmail.com>
2018-04-18 02:00:27 -0700

    added script for installing go.

---
+++ scripts/GoGetGo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Some variables that are likely to change often.
+VERSION=1.10.1
+OS=linux
+ARCH=amd64
+
+# Download the archived Go.
+sudo curl https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+
+# Unpack the Archive, placing it in /usr/local.
+sudo tar -C /usr/local -xzf go$VERSION.$OS-$ARCH.tar.gz
+
+# Add go to the path.
+cat <<EOT >> ~/.profile
+export PATH=$PATH:/usr/local/go/bin
+EOT
