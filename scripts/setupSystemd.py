61e1488b048fe929e5da26f4320133377bddca20
fractalbach <4bach@protonmail.com>
2018-04-19 07:24:08 -0700

    testing systemctl; updated service file;

--- scripts/setupSystemd.py
+++ scripts/setupSystemd.py
@@ -21,6 +21,10 @@ TODO LATER:
 - Infer targetpath based on systemd or systemctl configuration
 - Make a .config file so that you don't need to input commands directly.
 
+CODE CLEANUP:
+- use less variables
+- make the object type more clear. (to avoid that Path vs. string problem)
+
 _____________________________________________________________________________
 """
 
@@ -75,6 +79,7 @@ def err(*args, **kwargs):
 # --------------------------------------------------------------------------
 # Input Confirmation
 #
+print("Input Path = " + input_path)
 if not input_pathObject.exists():
     err("Input Path does not exist.")
 if not os.path.isfile(input_path):
@@ -82,11 +87,10 @@ if not os.path.isfile(input_path):
 if not os.access(input_path, os.R_OK):
     err("Input File needs to have read permissions.")
 
-print("Input File Path = " + input_path)
-
 # --------------------------------------------------------------------------
 # Target Confirmation
 # 
+print("Output Path = " + target_path)
 if not Path(TARGET_DIR).exists():
     err("Target directory does not exist.  Cannot copy file.")
 
@@ -94,64 +98,58 @@ if not Path(TARGET_DIR).exists():
 # File Copying
 # 
 try:
-    print("Copying from", input_path, "to", target_path)
     copyfile(input_path, target_path)
 except Exception as e:
     raise e
 else:
     print("File successfully copied to", target_path)
 
-
 # --------------------------------------------------------------------------
-# Systemctl 
+# Helper Function
 #
-# - Start the service.
-# - Enable the service.
-# - TODO: Check if service is actually enabled correctly.
-
-
 # which(program) acts like the Bash command "which", and returns the path
 # at which the program is located. If it can't be found, it returns "None".
-#
+# 
 # Credits:
 # https://stackoverflow.com/questions/377017/test-if-executable-exists-in-python 
-# 
-# def which(program):
-#     def is_exe(fpath):
-#         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
-#     fpath, fname = os.path.split(program)
-#     if fpath:
-#         if is_exe(program):
-#             return program
-#     else:
-#         for path in os.environ["PATH"].split(os.pathsep):
-#             exe_file = os.path.join(path, program)
-#             if is_exe(exe_file):
-#                 return exe_file
-#     return None
 
+def which(program):
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+    return None
 
 # --------------------------------------------------------------------------
 # Check for Systemctl
 # 
-# def checkForSystemctl():
-#     x = which("systemctl")
-#     if x == None:
-#         print("Systemctl was not located in the OS path.  Exiting.")
-#         exit(1)
-#     print("Found systemctl at path:", x)
-# checkForSystemctl()
-
+def checkForSystemctl():
+    x = which("systemctl")
+    if x == None:
+        print("Systemctl was not located in the OS path.  Exiting.")
+        exit(1)
+    print("Found systemctl at path:", x)
+checkForSystemctl()
 
 # --------------------------------------------------------------------------
-# Start and Enable Service
+# Start & Enable Using Systemctl 
 #
-# try:
-#     call(["sudo", "systemctl", "start", "simpleServer"])
-#     call(["sudo", "systemctl", "enable", "simpleServer"])
-# except Exception as e:
-#     raise e
-
+# - Start the service.
+# - Enable the service.
+# - TODO: Check if service is actually enabled correctly.
+# 
+try:
+    call(["sudo", "systemctl", "start", "simpleServer"])
+    call(["sudo", "systemctl", "enable", "simpleServer"])
+except Exception as e:
+    raise e
 
 # --------------------------------------------------------------------------
 # Success.
